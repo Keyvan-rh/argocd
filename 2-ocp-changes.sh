55cf82e41421b3181abed3bacff9e72e057e44d1
keyvan pishevar <kpishevar@gmail.com>
2020-05-06 09:50:24 -0500

    update step 2

--- 2-ocp-changes.sh
+++ 2-ocp-changes.sh
@@ -1,7 +1,17 @@
 oc patch deployment argocd-dex-server -p '{"spec": {"template": {"spec": {"containers": [{"name": "dex","image": "quay.io/redhat-cop/dex:v2.22.0-openshift"}]}}}}'
-oc patch serviceaccount argocd-dex-server --type='json' -p='[{"op": "add", "path": "/metadata/annotations/serviceaccounts.openshift.io/oauth-redirecturi.argocd", "value":"https://argocd-argocd.apps.ocp4.keyvan.home/api/dex/callback"}]'
+oc patch serviceaccount argocd-dex-server --type='json' -p='[{"op": "add", "path": "/metadata/annotations/serviceaccounts.openshift.io~1oauth-redirecturi.argocd", "value":"https://argocd-argocd.apps.ocp4.keyvan.home/api/dex/callback"}]'
 echo "update argocd-cm and add the groups"
-oc adm groups add-users argocdusers keyvan
+echo "oc edit cm argocd-cm"
+echo " check the sample in arcgocd argocd-cm.yaml for section data and its values"
+oc adm groups new argocdusers
+oc adm groups new argocadmins
+oc adm groups add-users argocdadmin admin
 oc adm groups add-users argocdusers developer
 echo "update argocd-rbac-cmi and give users role"
+echo "oc edit cm argocd-rbac-cm"
+echo "data:
+  policy.csv: |
+    g, argocdusers, role:readonly
+    g, argocdadmins, role:admin"
+
  
